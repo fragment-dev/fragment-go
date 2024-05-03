86e79e34112314ff38b2b1d2c67d848128a8f923
Vigneshwer Vaidyanathan <vignesh.vaid@gmail.com>
2024-05-02 18:32:59 -0700

    Be consistent in exit codes in the example

--- example/main.go
+++ example/main.go
@@ -32,13 +32,15 @@ func main() {
 	if err != nil {
 		fmt.Println("Failed to create ledger.")
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
 
 	if respBytes, err := data.MarshalJSON(); err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	} else {
 		fmt.Println("Successfully created ledger.")
 		fmt.Println(string(respBytes))
+		return
 	}
 }
