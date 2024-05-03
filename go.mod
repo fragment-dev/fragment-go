814237690e838ff708215cf2d4de5d37068e37b0
Vigneshwer Vaidyanathan <vignesh.vaid@gmail.com>
2024-05-02 18:26:05 -0700

    Add programmatic codegen to the SDK

--- go.mod
+++ go.mod
@@ -2,6 +2,16 @@ module github.com/fragment-dev/fragment-go
 
 go 1.20
 
-require github.com/Khan/genqlient v0.7.0
+require (
+	github.com/Khan/genqlient v0.7.0
+	github.com/alexflint/go-arg v1.4.3
+)
 
-require github.com/vektah/gqlparser/v2 v2.5.11 // indirect
+require (
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/alexflint/go-scalar v1.1.0 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.11 // indirect
+	golang.org/x/mod v0.15.0 // indirect
+	golang.org/x/tools v0.18.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+// genqclient needs a schema file in the directory it is run in. This schema file is used to generate the client.
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/Khan/genqlient/generate"
+	"github.com/alexflint/go-arg"
+)
+
+// Download the schema.graphql file from the fragment API and use it to generate the client.
+// Then, remove it from the directory after the client is generated.
+type cliArgs struct {
+	PackageName string   `arg:"--package" default:"main" help:"The package name to use for the generated client."`
+	Inputs      []string `arg:"-i,--input,separate" help:"The input files to generate a client from."`
+	Output      string   `arg:"-o,--output" help:"The output file to write the generated client to."`
+}
+
+func downloadSchemaToTempFile() (string, error) {
+	req, err := http.NewRequest(http.MethodGet, "https://api.fragment.dev/schema.graphql", nil)
+	if err != nil {
+		return "", err
+	}
+
+	client := &http.Client{}
+	getResp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer getResp.Body.Close()
+
+	tempFile, err := os.CreateTemp("", "schema.graphql")
+	if err != nil {
+		return "", err
+	}
+	defer tempFile.Close()
+
+	_, err = io.Copy(tempFile, getResp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return tempFile.Name(), nil
+}
+
+func main() {
+	var args cliArgs
+	arg.MustParse(&args)
+
+	if len(args.Inputs) == 0 {
+		fmt.Println("No input files provided.")
+		os.Exit(1)
+	}
+
+	tempDir, err := os.MkdirTemp("", "*")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer os.RemoveAll(tempDir)
+
+	schemaFile, err := downloadSchemaToTempFile()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	codegenConfig := &generate.Config{
+		Schema:       []string{schemaFile},
+		ContextType:  "github.com/fragment-dev/fragment-go/auth.AuthenticatedContext",
+		ClientGetter: "github.com/fragment-dev/fragment-go/client.NewClient",
+		Operations:   args.Inputs,
+		Bindings: map[string]*generate.TypeBinding{
+			"AlphaNumericString":  {Type: "string"},
+			"Date":                {Type: "string"},
+			"DateTime":            {Type: "string"},
+			"Int64":               {Type: "string"},
+			"Int96":               {Type: "string"},
+			"JSON":                {Type: "encoding/json.RawMessage"},
+			"JSONObject":          {Type: "encoding/json.RawMessage"},
+			"LastMoment":          {Type: "string"},
+			"ParameterizedString": {Type: "string"},
+			"Period":              {Type: "string"},
+			"SafeString":          {Type: "string"},
+			"UTCOffset":           {Type: "string"},
+		},
+		Package:   args.PackageName,
+		Generated: args.Output,
+	}
+
+	generated, err := generate.Generate(codegenConfig)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for filename, content := range generated {
+		err = os.MkdirAll(filepath.Dir(filename), 0o755)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		err = os.WriteFile(filename, content, 0o644)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	fmt.Println("Successfully generated client to " + args.Output + ".")
+	return
+}
